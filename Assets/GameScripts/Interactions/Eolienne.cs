c4eae603125d5c3793699e7d75a755b4929e3420
Cyril KHALIL <c.khalil@student.isartdigital.com>
2023-04-11 16:02:19 +0200

    Interactables added, pressure plate and lever added and working

---
+++ Assets/GameScripts/Interactions/Eolienne.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eolienne : Interactibles
+{
+    public override void OnOff()
+    {
+        return;
+    }
+
+    public override void Update()
+    {
+    }
+}
