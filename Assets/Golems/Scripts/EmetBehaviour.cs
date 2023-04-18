66a4d296637ccb9496de6e591e3026d8a295a563
c.khalil <c.khalil@student.isartdigital.com>
2023-04-18 22:07:39 +0200

    added prefab and gd scenes

--- Assets/Golems/Scripts/EmetBehaviour.cs
+++ Assets/Golems/Scripts/EmetBehaviour.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
