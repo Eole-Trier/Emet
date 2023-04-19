90011e73b234aa0501395e8182962d878adebedc
Eole TRIER <e.trier@student.isartdigital.com>
2023-04-19 16:37:10 +0200

    Efrit burn progressing #1

--- Assets/Golems/Scripts/EfritBehaviour.cs
+++ Assets/Golems/Scripts/EfritBehaviour.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EfritBehaviour : Golem
 {
+    [SerializeField] private bool m_IsBurning;
+    [SerializeField] private float m_BurnDistance;
     
+    private int m_BurnableLayer;
+
 
     // Start is called before the first frame update
     void Start()
@@ -12,21 +17,31 @@ public class EfritBehaviour : Golem
         m_Type = Type.EFRIT;
         m_InitialJumpStrength = m_JumpStrength;
         m_InitialSpeed = m_Speed;
+        m_BurnableLayer = 1 << LayerMask.NameToLayer("Burnable");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_IsBurning)
+        {
+            Burn();
+        }
     }
 
     public override IEnumerator UseCapacity(double timePressed)
     {
-        Burn();
         yield return null;
     }
     private void Burn()
     {
+        Collider[] burn = Physics.OverlapSphere(transform.position, m_BurnDistance, m_BurnableLayer);
 
+        foreach (Collider c in burn)
+        {
+
+            Destroy(c.gameObject);
+        }
     }
 }
