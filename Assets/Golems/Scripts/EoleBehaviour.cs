9ef39104b3377b09301fb91c6c9d14425ec8760f
c.khalil <c.khalil@student.isartdigital.com>
2023-04-19 15:46:40 +0200

    modified gitignore

--- Assets/Golems/Scripts/EoleBehaviour.cs
+++ Assets/Golems/Scripts/EoleBehaviour.cs
@@ -53,18 +53,19 @@ public class EoleBehaviour : Golem
             if (collider.attachedRigidbody == null)
                 continue;
 
-            //if wind is in front of eole
+            Rigidbody rb = collider.attachedRigidbody;
             if (collider.TryGetComponent(out Golem golem) && collider.name == m_PlayerSwitch.golems[m_PlayerSwitch.m_CurrentGolem].name)
             {
+                //if wind is in front of eole
                 if (forward)
                 {
                     if (m_PlayerMovement.GetMoveDirection() == Vector3.zero)
                     {
-                        golem.GetComponent<Rigidbody>().AddForce((transform.forward * m_WindForceHorizontal) * 12);
+                        rb.AddForce((transform.forward * m_WindForceHorizontal) * 12);
                     }
                     else
                     {
-                        golem.GetComponent<Rigidbody>().AddForce((transform.forward * m_WindForceHorizontal) * 4);
+                        rb.AddForce((transform.forward * m_WindForceHorizontal) * 4);
                     }
                 }
 
@@ -73,20 +74,20 @@ public class EoleBehaviour : Golem
                 {
                     if (m_PlayerMovement.GetMoveDirection() == Vector3.zero)
                     {
-                        golem.GetComponent<Rigidbody>().AddForce((transform.up * m_WindForceVertical) * 8);
+                        rb.AddForce((transform.up * m_WindForceVertical) * 8);
                     }
                     else
                     {
-                        golem.GetComponent<Rigidbody>().AddForce((transform.up * m_WindForceVertical) * 4);
+                        rb.AddForce((transform.up * m_WindForceVertical) * 4);
                     }
                 }
             }
             else
             {
-                if(forward)
-                    collider.attachedRigidbody.AddForce(transform.forward * m_WindForceHorizontal);
+                if (forward)
+                    rb.AddForce(transform.forward * m_WindForceHorizontal);
                 else
-                    collider.attachedRigidbody.AddForce(transform.up * m_WindForceVertical);
+                    rb.AddForce(transform.up * m_WindForceVertical);
             }
         }
     }
