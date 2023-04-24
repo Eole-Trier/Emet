f83e569c84f7d831f1fa246f7c4d4f8a82630657
c.khalil <c.khalil@student.isartdigital.com>
2023-04-24 13:55:15 +0200

    added checkpoints and camera changes

---
+++ Assets/Scripts/GP/Golems/Scripts/EnkiBehaviour.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EnkiBehaviour : Golem
+{
+    [SerializeField] private float m_TimeBeforeIdle;
+    private Rigidbody m_RigidBody;
+    private float m_IdleTimer;
+    private bool m_Freezed;
+    private List<BoxCollider> m_BoxCollider;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_BoxCollider = new(GetComponents<BoxCollider>());
+        m_Type = GolemType.ENKI;
+        m_PlayerMovement = FindObjectOfType<PlayerMovement>();
+        m_RigidBody = GetComponent<Rigidbody>();
+        m_Freezed = false;
+        m_CancelAnimator = false;
+        m_InitialJumpStrength = m_JumpStrength;
+        m_InitialSpeed = m_Speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (m_PlayerMovement.IsGrounded)
+        {
+            foreach(var collider in m_BoxCollider)
+            {
+                collider.material = null;
+            }
+        }
+        else
+        {
+            foreach (var collider in m_BoxCollider)
+            {
+                collider.material = PhysicMaterial;
+            }
+        }
+        /*if (m_PlayerMovement.GetMoveDirection() == Vector3.zero && !m_Freezed)
+        {
+            m_IdleTimer -= Time.fixedDeltaTime;
+            if (m_IdleTimer <= 0)
+            {
+                m_PlayerMovement.GetAnimator().Play("Idle");
+                m_IdleTimer = m_TimeBeforeIdle;
+            }
+        }
+        else
+            m_IdleTimer = m_TimeBeforeIdle;*/
+    }
+
+    public override IEnumerator UseCapacity(double timePressed)
+    {
+        if (!m_Freezed)
+        {
+            m_RigidBody.constraints = RigidbodyConstraints.FreezeAll;
+            m_Freezed = true;
+            m_PlayerMovement.SetMoveDirection(Vector3.zero);
+            m_CancelAnimator = true;
+            m_BoxCollider.ForEach((box) => box.enabled ^= true);
+            m_PlayerMovement.GetAnimator().Play("EnkiPlateform");
+        }
+        else
+        {
+            m_RigidBody.constraints = RigidbodyConstraints.FreezeRotation;
+            m_Freezed = false;
+            m_CancelAnimator = false;
+            m_BoxCollider.ForEach((box) => box.enabled ^= true);
+            m_PlayerMovement.GetAnimator().Play("EnkiGolem");
+        }
+        yield return null;
+    }
+
+    public bool IsFreezed() => m_Freezed;
+}
---
+++ Assets/Scripts/GP/Golems/Scripts/EoleBehaviour.cs
@@ -0,0 +1,145 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
+
+public class EoleBehaviour : Golem
+{
+    [HideInInspector] public List<Collider> listCollider = new();
+    [SerializeField] private float m_WindForceHorizontal;
+    [SerializeField] private float m_WindForceVertical;
+    [SerializeField] private float m_TimeBeforeIdle;
+    [HideInInspector] public bool windActive;
+    private List<CapsuleCollider> m_WindCollider = new();
+    private PlayerSwitch m_PlayerSwitch;
+    private float m_IdleTimer;
+    private bool forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        forward = true;
+        m_Type = GolemType.EOLE;
+        m_CancelAnimator = false;
+        windActive = false;
+        m_PlayerSwitch = FindObjectOfType<PlayerSwitch>();
+        m_PlayerMovement = FindObjectOfType<PlayerMovement>();
+        GetComponents(m_WindCollider);
+        if (m_WindCollider[0].enabled == true && m_WindCollider[1].enabled == true)
+        {
+            if (m_WindCollider[0].direction == 2)
+            {
+                m_WindCollider[0].enabled = true;
+                m_WindCollider[1].enabled = false;
+            }
+            else
+            {
+                m_WindCollider[0].enabled = false;
+                m_WindCollider[1].enabled = true;
+            }
+        }
+        m_InitialJumpStrength = m_JumpStrength;
+        m_InitialSpeed = m_Speed;
+    }
+
+    private void FixedUpdate()
+    {
+        BoxCollider c = GetComponent<BoxCollider>();
+        if (m_PlayerMovement.IsGrounded)
+        {
+            c.material = null;
+        }
+        else
+        {
+            c.material = PhysicMaterial;
+        }
+    }
+    // Update is called once per frame
+    public void EoleUpdate()
+    {
+        foreach (Collider collider in listCollider)
+        {
+            //if there is no rigidbody on collider we leave
+            if (collider.attachedRigidbody == null)
+                continue;
+
+            Rigidbody rb = collider.attachedRigidbody;
+            if (collider.TryGetComponent(out Golem golem) && collider.name == m_PlayerSwitch.Rooms[m_PlayerSwitch.m_CurrentRoom].Golems[m_PlayerSwitch.m_CurrentGolem].name)
+            {
+                //if wind is in front of eole
+                if (forward)
+                {
+                    if (m_PlayerMovement.GetMoveDirection() == Vector3.zero)
+                    {
+                        rb.AddForce((transform.forward * m_WindForceHorizontal) * 12);
+                    }
+                    else
+                    {
+                        rb.AddForce((transform.forward * m_WindForceHorizontal) * 4);
+                    }
+                }
+
+                //if wind is above eole
+                else
+                {
+                    if (m_PlayerMovement.GetMoveDirection() == Vector3.zero)
+                    {
+                        rb.AddForce((transform.up * m_WindForceVertical) * 8);
+                    }
+                    else
+                    {
+                        rb.AddForce((transform.up * m_WindForceVertical) * 4);
+                    }
+                }
+            }
+            else
+            {
+                if (forward)
+                    rb.AddForce(transform.forward * m_WindForceHorizontal);
+                else
+                    rb.AddForce(transform.up * m_WindForceVertical);
+            }
+        }
+    }
+
+    public override IEnumerator UseCapacity(double timePressed)
+    {
+        listCollider.Clear();
+        m_WindCollider[0].enabled ^= true;
+        m_WindCollider[1].enabled ^= true;
+        if (m_PlayerMovement.IsGrounded && m_PlayerMovement.GetMoveDirection() == Vector3.zero)
+        {
+            forward ^= true;
+            if (!forward)
+            {
+                m_PlayerMovement.GetAnimator().Play("EoleVertical");
+                m_PlayerMovement.GetAnimator().SetBool("LookingUP", true);
+            }
+            else if (forward)
+            {
+                m_PlayerMovement.GetAnimator().Play("EoleHorizontal");
+                m_PlayerMovement.GetAnimator().SetBool("LookingUP", false);
+            }
+        }
+        windActive ^= true;
+        yield return null;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!listCollider.Contains(other))
+        {
+            listCollider.Add(other);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (listCollider.Contains(other))
+        {
+            listCollider.Remove(other);
+        }
+    }
+}
