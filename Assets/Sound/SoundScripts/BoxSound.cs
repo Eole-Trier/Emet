d3a8ca9b03806ed38e8ce21bedec8428276f1608
c.khalil <c.khalil@student.isartdigital.com>
2023-04-26 15:52:53 +0200

    fixed building errors

--- Assets/Sound/SoundScripts/BoxSound.cs
+++ Assets/Sound/SoundScripts/BoxSound.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.VersionControl;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class BoxSound : MonoBehaviour
 {
