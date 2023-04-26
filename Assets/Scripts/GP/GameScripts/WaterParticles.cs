7bd4a99705ec1c1f01ee8b646aea4e13a68a87f6
c.khalil <c.khalil@student.isartdigital.com>
2023-04-26 14:34:56 +0200

    modified waterparticles script

--- Assets/Scripts/GP/GameScripts/WaterParticles.cs
+++ Assets/Scripts/GP/GameScripts/WaterParticles.cs
@@ -22,8 +22,6 @@ public class WaterParticles : MonoBehaviour
                 timer = m_TimerToShutDown;
                 burningObject.IsBurning = false;
             }
-            else
-                timer -= Time.fixedDeltaTime;
         }
     }
 }
