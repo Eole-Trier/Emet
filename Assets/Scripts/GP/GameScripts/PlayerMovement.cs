5833b9a989db1301bb7665fb7aa2a8bab171fbe2
c.khalil <c.khalil@student.isartdigital.com>
2023-04-26 15:29:39 +0200

    changes to usings

--- Assets/Scripts/GP/GameScripts/PlayerMovement.cs
+++ Assets/Scripts/GP/GameScripts/PlayerMovement.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
 using static UnityEditor.PlayerSettings;
 
 public class PlayerMovement : MonoBehaviour
