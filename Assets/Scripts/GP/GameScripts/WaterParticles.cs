be825bf6055b5b033a7787cf25872437062895ee
Angela Quay <a.quay@student.isartdigital.com>
2023-04-26 11:58:39 +0200

    Details on prefab Efrit and lvl 4

---
+++ Assets/Scripts/GP/GameScripts/WaterParticles.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class WaterParticles : MonoBehaviour
+{
+    [SerializeField] private float m_TimerToShutDown;
+    private float timer;
+
+    private void Start()
+    {
+        timer = m_TimerToShutDown;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if(other.TryGetComponent(out BurningObject burningObject) && burningObject.IsBurning)
+        {
+            if (timer <= 0)
+            {
+                timer = m_TimerToShutDown;
+                burningObject.IsBurning = false;
+            }
+            else
+                timer -= Time.fixedDeltaTime;
+
+        }
+    }
+}
