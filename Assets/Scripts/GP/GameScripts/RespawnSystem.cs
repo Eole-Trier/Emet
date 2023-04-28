001efa40764fde23ab343f0bd3ab172a193034df
Eole TRIER <e.trier@student.isartdigital.com>
2023-04-28 23:47:29 +0200

    Respawn timer added

--- Assets/Scripts/GP/GameScripts/RespawnSystem.cs
+++ Assets/Scripts/GP/GameScripts/RespawnSystem.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class RespawnSystem : MonoBehaviour
 {
+    [SerializeField] private float m_RespawnTime;
     private void OnTriggerEnter(Collider other)
     {
         if (other is BoxCollider && other.gameObject.TryGetComponent(out ObjectType obj))
         {
             if (obj.ObjType.HasFlag(ObjectType.Type.Respawn))
             {
+                if (obj.TryGetComponent(out Golem golem))
+                {
+                    Die();
+                }
                 obj.transform.position = obj.InitialPosition;
             }
         }
     }
+
+    private IEnumerator Die()
+    {
+        // add sound
+        yield return new WaitForSeconds(m_RespawnTime);
+    }
 }
+
