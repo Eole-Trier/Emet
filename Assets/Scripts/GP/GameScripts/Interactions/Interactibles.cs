83a6ee1d4820989e77e1ea5d65279420c58a6294
c.khalil <c.khalil@student.isartdigital.com>
2023-04-28 17:33:03 +0200

    fixed interactible script error

--- Assets/Scripts/GP/GameScripts/Interactions/Interactibles.cs
+++ Assets/Scripts/GP/GameScripts/Interactions/Interactibles.cs
@@ -15,7 +15,13 @@ public abstract class Interactibles : MonoBehaviour
     {
         foreach (Mechanism m in MechanismList)
         {
-            if(!m.m_InteractibleList.Contains(this)) 
+            if (m == null)
+            {
+                Debug.Log("Mechanism missing in : " + name);
+                continue;
+            }
+
+            else if (!m.m_InteractibleList.Contains(this))
             {
                 m.m_InteractibleList.Add(this);
             }
@@ -26,13 +32,18 @@ public abstract class Interactibles : MonoBehaviour
     {
         foreach (Mechanism m in MechanismList)
         {
+            // If every interactibles the object is on then activate/desactivate it
             if (m.timer == 0 && m.gameObject.activeInHierarchy == m.IsActive && m.m_InteractibleList.FindAll(interactible => interactible.IsOn).Count == m.m_InteractibleList.Count)
                 m.gameObject.SetActive(!m.IsActive);
+
+            // If timer is at 0 and every interactibles the object is on then activate/desactivate
             if (m.myTimer <= 0 && m.timer > 0 && m.m_InteractibleList.FindAll(interactible => interactible.IsOn).Count == m.m_InteractibleList.Count)
-            {
                 m.gameObject.SetActive(!m.gameObject.activeInHierarchy);
+
+            //If playOnce is true then loop the activate/desactivate state if timer > 0
+            if (m.myTimer <= 0 && !m.playOnce)
                 m.myTimer = m.timer;
-            }
+
             m.myTimer -= Time.fixedDeltaTime;
         }
     }
@@ -40,6 +51,9 @@ public abstract class Interactibles : MonoBehaviour
     {
         foreach (Mechanism m in MechanismList)
         {
+            if (m == null)
+                continue;
+
             m.myTimer = m.timer;
             if (m.gameObject.activeInHierarchy != m.IsActive && m.m_InteractibleList.FindAll(interactible => interactible.IsOn).Count == 0)
                 m.gameObject.SetActive(m.IsActive);
--- Assets/Scripts/GP/GameScripts/Interactions/Mechanism.cs
+++ Assets/Scripts/GP/GameScripts/Interactions/Mechanism.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Mechanism : MonoBehaviour
 {
     public float timer;
+    public bool playOnce;
     [HideInInspector] public float myTimer;
     [HideInInspector] public bool IsActive;
     [HideInInspector] public List<Interactibles> m_InteractibleList;
