a8288a7fa634afd470d4bb3ae6ab6daafd3e26c8
Eole TRIER <e.trier@student.isartdigital.com>
2023-04-29 12:35:16 +0200

    Pressure plate accurate ++

--- Assets/Scripts/GP/GameScripts/Interactions/PressurePlate.cs
+++ Assets/Scripts/GP/GameScripts/Interactions/PressurePlate.cs
@@ -10,10 +10,10 @@ public class PressurePlate : Interactibles
 
     public override void FixedUpdate()
     {
-        IsOn = Physics.Raycast(transform.position + Vector3.down * 0.05f, Vector3.up, 0.25f);
+        IsOn = Pressed();
         if (IsOn)
             Active();
-        
+
         else
             Desactive();
 
@@ -27,5 +27,28 @@ public class PressurePlate : Interactibles
             m_AudioManager.Play("pressurePlate_off");
             m_Played = false;
         }
+    }
+    public bool Pressed()
+    {
+        Vector3 P1 = new(transform.position.x + transform.localScale.x / 2, transform.position.y, transform.position.z + transform.localScale.z / 2);
+        Vector3 P2 = new(transform.position.x + transform.localScale.x / 2, transform.position.y, transform.position.z - transform.localScale.z / 2);
+        Vector3 P3 = new(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z + transform.localScale.z / 2);
+        Vector3 P4 = new(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z - transform.localScale.z / 2);
+        Vector3 P5 = transform.position;
+        Debug.DrawRay(P1 + Vector3.down * 0.05f, Vector3.up, Color.red);
+        Debug.DrawRay(P2 + Vector3.down * 0.05f, Vector3.up, Color.red);
+        Debug.DrawRay(P3 + Vector3.down * 0.05f, Vector3.up, Color.red);
+        Debug.DrawRay(P4 + Vector3.down * 0.05f, Vector3.up, Color.red);
+        Debug.DrawRay(P5 + Vector3.down * 0.05f, Vector3.up, Color.red);
+
+
+        if (   Physics.Raycast(P5 + Vector3.down * 0.05f, Vector3.up, 0.25f)
+            || Physics.Raycast(P1 + Vector3.down * 0.05f, Vector3.up, 0.25f)
+            || Physics.Raycast(P2 + Vector3.down * 0.05f, Vector3.up, 0.25f)
+            || Physics.Raycast(P3 + Vector3.down * 0.05f, Vector3.up, 0.25f)
+            || Physics.Raycast(P4 + Vector3.down * 0.05f, Vector3.up, 0.25f))
+            return true;
+
+        return false;
     }
 }
