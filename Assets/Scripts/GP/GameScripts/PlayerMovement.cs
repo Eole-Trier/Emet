ae859cd48add0a9ebc74e432e2f3114a17e114dc
c.khalil <c.khalil@student.isartdigital.com>
2023-04-30 10:52:25 +0200

    Animator fixed

--- Assets/Scripts/GP/GameScripts/PlayerMovement.cs
+++ Assets/Scripts/GP/GameScripts/PlayerMovement.cs
@@ -6,8 +6,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private AudioListener m_AudioListener;
-    public Animator m_Animator;
     [SerializeField] private float m_RangeToActivate;
     [SerializeField] private float TimeBeforePlay;
     [SerializeField] private float m_WalkingSoundTimer;
@@ -17,10 +15,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody m_Rigidbody;
     private Transform m_GolemTransform;
     private Golem m_Golem;
+    private Animator m_Animator;
     private AudioManager m_AudioManager;
+    private AudioListener m_AudioListener;
     public Vector3 m_MoveDirection;
     private float m_Timer;
-    private bool m_IsMoving { get { return m_MoveDirection != Vector3.zero; } }
+    public bool IsMoving { get { return m_MoveDirection != Vector3.zero; } }
 
     // Start is called before the first frame update
     IEnumerator Start()
@@ -35,8 +35,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Movement();
         m_AudioListener.transform.position = transform.position;
         m_AudioListener.transform.Rotate(new Vector3(0, 180, 0));
         IsGrounded = Grounded();
@@ -45,14 +46,7 @@ public class PlayerMovement : MonoBehaviour
         if (m_Golem.m_CancelAnimator != false)
             return;
 
-        m_Animator.SetFloat("SpeedX", m_MoveDirection.x);
-        m_Animator.SetFloat("SpeedY", m_Rigidbody.velocity.y);
-        if(IsGrounded)
-            m_Animator.SetFloat("SpeedY", 0);
-        m_Animator.SetFloat("SpeedZ", m_MoveDirection.z);
-        m_Animator.SetBool("Moving", m_IsMoving);
-
-        if (m_IsMoving && IsGrounded)
+        if (IsMoving && IsGrounded)
         {
             if (m_Timer <= 0)
             {
@@ -68,11 +62,6 @@ public class PlayerMovement : MonoBehaviour
             m_GolemTransform.rotation = Quaternion.RotateTowards(m_GolemTransform.rotation, toRotation, 720 * Time.deltaTime);
             
         }
-    }
-
-    private void FixedUpdate()
-    {
-        Movement();
     }
 
     private bool Grounded()
@@ -206,6 +195,8 @@ public class PlayerMovement : MonoBehaviour
 
     public void SetMoveDirection(Vector3 move) => m_MoveDirection = move;
     public Vector3 GetMoveDirection() => m_MoveDirection;
+
+    public Rigidbody GetRigidbody() => m_Rigidbody;
 
     public Animator GetAnimator() => m_Animator;
 }
