562f01f5512cfd8a417f58ae78bfb010e596079f
Eole TRIER <e.trier@student.isartdigital.com>
2023-05-01 13:25:58 +0200

    Checkpoint fixed

--- Assets/Scripts/GP/GameScripts/Checkpoint.cs
+++ Assets/Scripts/GP/GameScripts/Checkpoint.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    [SerializeField] private new GameObject gameObject;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent(out Golem golem))
         {
             if (golem.gameObject.TryGetComponent(out ObjectType obj))
             {
-                gameObject.SetActive(true);
                 obj.InitialPosition = obj.transform.position;
             }
         }
